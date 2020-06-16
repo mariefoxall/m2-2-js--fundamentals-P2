4cfe3c52b9d2dc8c19298f9b58aa5fe569951ec9
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 16:03:21 -0400

    Exercise 2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -5,11 +5,11 @@
 // Define the function
 function max(num1, num2) {
   if (num1 > num2) {
-    return num1;
+    console.log(num1);
   } else if (num2 > num1) {
-    return num2;
+    console.log(num2);
   }
 }
 
 // Call the function
-console.log(max(12, 43));
+max(12, 43);
