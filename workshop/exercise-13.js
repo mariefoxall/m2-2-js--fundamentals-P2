cefa44fe152bc9ec0de594ee22eff427d2115caf
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 19:08:55 -0400

    Exercise 13

--- workshop/exercise-13.js
+++ workshop/exercise-13.js
@@ -9,6 +9,14 @@
 // and returns a number that indicates how many uppercase “B”
 // characters there are in the string.
 
-function countBs(str) {}
+function countBs(str) {
+  let count = 0;
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === "B") {
+      count = count + 1;
+    }
+  }
+  return count;
+}
 // -------------------------------------------------------------------------
-console.log('Q1: ', countBs('BananaBabyBubbles'));
+console.log("Q1: ", countBs("BananaBabyBubbles"));
