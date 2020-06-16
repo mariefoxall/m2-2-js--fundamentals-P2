c3768e0834d364644eeafb273a4d266a2e7d7d97
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 18:46:26 -0400

    Exercise 10

--- workshop/exercise-10.js
+++ workshop/exercise-10.js
@@ -7,11 +7,14 @@
 // Returns true if every element of lst is of length at least 5.
 // Otherwise returns false.
 function allLong(lst) {
-  // lst is an array of strings
+  let check = lst.every(function (element) {
+    return element.length > 4;
+  });
+  return check;
 }
 // -------------------------------------------------------------------------
-console.log('Q8 - case 1', allLong(['Scott', 'Bob', 'Ric', 'Jim']));
+console.log("Q10 - case 1", allLong(["Scott", "Bob", "Ric", "Jim"]));
 console.log(
-  'Q8 - case 2',
-  allLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash'])
+  "Q10 - case 2",
+  allLong(["Cyborg", "Robin", "Batman", "Superman", "Aquaman", "Flash"])
 );
