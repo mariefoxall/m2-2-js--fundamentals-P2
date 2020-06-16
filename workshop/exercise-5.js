c51ec51ceb811fedb01e92d4f696b7333fd5a42b
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 16:32:20 -0400

    Exercise 5

--- workshop/exercise-5.js
+++ workshop/exercise-5.js
@@ -2,19 +2,25 @@
 // Write a function that takes an array and a value as arguments and
 // returns true if the value is in the array else it returns false.
 
-function itemIsPresent(array, item) {}
+function itemIsPresent(array, item) {
+  if (array.indexOf(item) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Test case
 const myArray = [
-  'bacon',
-  'purple',
-  'door',
-  'window',
-  'exist',
-  'code',
-  'program',
-  'funky',
+  "bacon",
+  "purple",
+  "door",
+  "window",
+  "exist",
+  "code",
+  "program",
+  "funky",
 ];
 
-console.log(itemIsPresent(myArray, 'funky')); // true
-console.log(itemIsPresent(myArray, 'elephant')); // false
+console.log(itemIsPresent(myArray, "funky")); // true
+console.log(itemIsPresent(myArray, "elephant")); // false
