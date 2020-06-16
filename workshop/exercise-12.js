1d151ece3eeb9219ad4a69cc2d58ce9fba1c40c1
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 19:03:15 -0400

    Exercise 12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -1,5 +1,5 @@
 // Q12
-// Build on what you did for Q9.
+// Build on what you did for Q11.
 // This time the function should return a letter grade instead of the average.
 // Here is how I calculate the grades
 // <60 	is F
@@ -9,6 +9,26 @@
 // <100 is A
 
 // Write function here
+function calculateAverage(grades) {
+  let sum = 0;
+  let total = grades.forEach(function (element) {
+    sum = sum + element;
+  });
+  let average = Math.round(sum / grades.length);
+  if (average < 60) {
+    return "F";
+  } else if (average < 70) {
+    return "D";
+  } else if (average < 80) {
+    return "C";
+  } else if (average < 90) {
+    return "B";
+  } else if (average < 100) {
+    return "A";
+  }
+}
+
+console.log(calculateAverage([48, 95, 65, 48, 59, 78, 72, 65]));
 
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
