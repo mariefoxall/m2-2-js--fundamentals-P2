3a6e67721f429e5cea8cc896b665b81b9894eea7
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 16:13:16 -0400

    Exercise 3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,6 +2,14 @@
 // Write a function that accepts two integers and returns
 // the greater one. (this is slightly different from the previous question.)
 
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  }
+}
+
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
 
@@ -9,5 +17,14 @@
 // Without changing the function at all,
 // Can you figure out how we could use it to determine the greater number between 4 integers?
 
+let max1 = max(numA, numB);
+let max2 = max(numC, numD);
+
+let maxAll = max(max1, max2);
+
+console.log(maxAll);
+
 // Q3.3
 // Would this work with more integers?
+
+// Yes, but you'd have to keep redefining and nesting the function and it would get very tedious.
