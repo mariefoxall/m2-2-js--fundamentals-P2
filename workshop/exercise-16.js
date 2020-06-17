266fedf24b1b2a2388b6a531252bafe59411065e
Marie Foxall <marie@wastedeffort.ca>
2020-06-16 20:04:17 -0400

    Exercise 16

--- workshop/exercise-16.js
+++ workshop/exercise-16.js
@@ -10,6 +10,14 @@
 
 let armstrongNumbers = [];
 
-// write your loop here...
+for (a = 0; a <= 9; a++) {
+  for (b = 0; b <= 9; b++) {
+    for (c = 0; c <= 9; c++) {
+      if (a * a * a + b * b * b + c * c * c === a * 100 + b * 10 + c) {
+        armstrongNumbers.push(a * 100 + b * 10 + c);
+      }
+    }
+  }
+}
 
 console.log(armstrongNumbers);
